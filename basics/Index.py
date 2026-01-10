7e981705bb631a05da6bc1de186763e665f40fe2
09parthchauhan <09parthchauhan@gmail.com>
2026-01-10 13:04:26 +0530

    revising strings..

--- basics/Index.py
+++ basics/Index.py
@@ -1,1 +1,25 @@
 print("doing revision......")
+
+#Strings....
+message = 'hello there!!!!'
+print(message)
+
+long_message = """hello I'am parth chauhan
+looking to build a career in software engineering.
+"""
+
+print(message[0:5])
+print(len(long_message))
+print(long_message[0:25])
+
+print(message.upper())
+print(long_message.find('chauhan'))
+
+greeting = "hello"
+name = "parth"
+
+#msg = greeting + ' ' + name
+
+msg = f'{greeting} {name}'
+print(msg)
+
