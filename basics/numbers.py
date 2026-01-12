199fe411376d883a64720b662b59f78bf9d0c421
09parthchauhan <09parthchauhan@gmail.com>
2026-01-12 16:05:51 +0530

    casting

--- basics/numbers.py
+++ basics/numbers.py
@@ -5,3 +5,15 @@ print(type(pi))
 
 print(13 % 4)
 
+print(2**2)
+print(3 * (2+1))
+
+print(3 * 2 + 1)
+
+name =  'parth'
+
+print(str(number) + " " + name) #can cast string to integer
+
+print('/n')
+print(number + " " + int(name)) #cannot cast integer to string
+
