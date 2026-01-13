044627b1ebf484f565bda544d81096eeebbdb5d2
09parthchauhan <09parthchauhan@gmail.com>
2026-01-13 10:27:49 +0530

    lists....

---
+++ basics/lists.py
@@ -0,0 +1,45 @@
+courses = ['AI', 'ML', 'TOC', 'CD']
+numbers = [34, 201, 1 ,10 ,45,6,266]
+
+print(courses)
+print(len(courses))
+print(courses[3])
+print(courses[-1])
+
+print(courses[:2])
+
+#add item to list
+courses.append('Backend')
+print(courses)
+
+#add item on specific location
+courses.insert(1, 'CN')
+print(courses)
+
+subjects = ['frontend', 'maths']
+
+courses.extend(subjects)
+print(courses)
+
+#remove items
+courses.remove('frontend')
+print(courses)
+
+
+popped = [courses.pop()]
+print(courses)
+print(popped)
+
+courses.reverse()
+print(courses)
+
+courses.sort()
+print(courses)
+
+#numbers.sort()
+#print(numbers)
+
+sorted_numbers = sorted(numbers)
+print(numbers)
+print(sorted_numbers)
+
