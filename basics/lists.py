889f282dc04c563582f188a1130f7d8bdf8debed
09parthchauhan <09parthchauhan@gmail.com>
2026-01-13 10:52:42 +0530

    updated lists

--- basics/lists.py
+++ basics/lists.py
@@ -1,12 +1,12 @@
 courses = ['AI', 'ML', 'TOC', 'CD']
 numbers = [34, 201, 1 ,10 ,45,6,266]
 
-print(courses)
-print(len(courses))
-print(courses[3])
-print(courses[-1])
+#print(courses)
+# print(len(courses))
+# print(courses[3])
+# print(courses[-1])
 
-print(courses[:2])
+# print(courses[:2])
 
 #add item to list
 courses.append('Backend')
@@ -43,3 +43,8 @@ sorted_numbers = sorted(numbers)
 print(numbers)
 print(sorted_numbers)
 
+print(courses.index('ML'))
+
+for item in enumerate(courses, start=1):
+    print(item)
+    
