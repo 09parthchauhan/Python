40c6b99495ba9b0191a1892902438dcf4e1c1c3f
09parthchauhan <09parthchauhan@gmail.com>
2026-01-18 09:54:55 +0530

    updated statements..

--- basics/statements.py
+++ basics/statements.py
@@ -1,8 +1,27 @@
-language = "Python"
+language = "Java"
 
-if language == "python":
+if language == "Python":
     print("Its True")
+elif language == "Java":
+    print("Yes it is!")
 else: 
-    print("nope your wrong")
+    print("nope your wrong") 
 
-    
+
+usr = 'Admin'
+logged_in = False
+
+if not logged_in:
+    print("Please login")
+    logged_in = True
+else:
+    print("welcome admin")
+
+
+a = [1,2,3]
+b = [1,2,3]
+
+print(id(a))
+print(id(b))
+print(a is b)
+
