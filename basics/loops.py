674fd8344fce8cf8209c28e8f8a99e756dc7b0a9
09parthchauhan <09parthchauhan@gmail.com>
2026-01-18 16:12:05 +0530

    loops..

---
+++ basics/loops.py
@@ -0,0 +1,27 @@
+numbers = [1,2,4,5,6,7]
+
+for num in numbers:
+    # if num == 4:
+    #     print(f"found the number: {num}")
+    #     #break
+    #     continue
+    # print(num)
+    
+    # for letter in 'abc':
+    #     print(num, letter)
+    pass
+
+# for i in range(1,11):
+#     print(i)
+#     pass
+
+
+x = 0
+while x <= 10:
+    if x == 5:
+        print(f"the number {x} is founded")
+        break
+    print(x)
+    x = x + 1
+
+
