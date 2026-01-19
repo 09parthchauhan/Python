95f67e085b4d674ad6e7099fec815d1c7e4d6020
09parthchauhan <09parthchauhan@gmail.com>
2026-01-19 12:27:48 +0530

    updated functions

--- basics/func.py
+++ basics/func.py
@@ -1,14 +1,26 @@
-def hello_func(name):
-    print("hello world")
-    profile = {
-        'name' : 'parth chauhan',
-        'age' : 23,
-        'email' : '09parthchauhan'
-    }
+# def hello_func(name):
+#     print("hello world")
+#     profile = {
+#         'name' : 'parth chauhan',
+#         'age' : 23,
+#         'email' : '09parthchauhan'
+#     }
 
-    for keys, values in profile.items():
-        print(f'{keys} : {values}')
+#     for keys, values in profile.items():
+#         print(f'{keys} : {values}')
 
-    return f"hello {name}"
-print(hello_func('parth').upper())
+#     return f"hello {name}"
+
+# print(hello_func('parth').upper())
+
+def student(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+courses = ['AI', 'ML']
+info = {'name':"parth", 'age' : 23}
+
+student(*courses, **info)
+#student('AI', 'Ml', name="parth", age = 23)
+
 
