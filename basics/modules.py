756453190ac472a979eb0c5b318b73a94c3002a2
09parthchauhan <09parthchauhan@gmail.com>
2026-01-20 10:04:11 +0530

    updated modules

--- basics/modules.py
+++ basics/modules.py
@@ -1,10 +1,13 @@
 #import my_module as mm
 from my_module import find_index, test
+import sys
 
 courses = ['AL', 'ML', 'TOC', 'CN', "WEB"]
 
 index = find_index(courses, 'WEB')
 print(index)
 print(test)
+
+print(sys.path)
 
 
