9c75815ee83dddfb5c9304df206df998292ca514
09parthchauhan <09parthchauhan@gmail.com>
2026-01-22 10:18:10 +0530

    random module

--- basics/modules.py
+++ basics/modules.py
@@ -1,11 +1,23 @@
 #import my_module as mm
-from my_module import find_index, test
-import sys
+# from my_module import find_index, test
+# import sys
+import random 
 
 courses = ['AL', 'ML', 'TOC', 'CN', "WEB"]
 
-index = find_index(courses, 'WEB')
-print(index)
-print(test)
 
-print(sys.path)
+x = 1
+while x<=20:
+    random_courses = random.choice(courses)
+    print(f"{random_courses}:{x}")
+    x += 1
+    if x==10:
+        #(f"{random_courses} {x}")
+        print(f"Breaking point...")
+        break
+
+# index = find_index(courses, 'WEB')
+# print(index)
+# print(test)
+
+# print(sys.path)
