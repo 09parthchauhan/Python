25f04ac5c3e0c556f813fc7a576d0d15b8cb2d11
09parthchauhan <09parthchauhan@gmail.com>
2026-01-27 18:06:02 +0530

    urls,views, and templates

--- django_project/core/todos/urls.py
+++ django_project/core/todos/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello_world_view, name='hello_world')
+    path('hello', views.hello_world_view, name='hello_world'),
+    path('', views.hello_name, name="hello_name"),
+    path('html', views.hello_html, name="hello_html")
 ]
+
 
--- django_project/core/todos/views.py
+++ django_project/core/todos/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def hello_world_view(request):
     return HttpResponse('Hello World!')
 
+def hello_name(request):
+    return HttpResponse("Parth Chauhan")
+
+def hello_html(request):
+    return render(request, 'todos/hello.html')
+
