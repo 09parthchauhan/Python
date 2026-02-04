7747f4278bb70a6bb73f95eb77a054b03937cf71
09parthchauhan <09parthchauhan@gmail.com>
2026-02-04 13:30:37 +0530

    queries & Get requests

--- django_project/core/todos/urls.py
+++ django_project/core/todos/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('hello', views.hello_world_view, name='hello_world'),
     path('', views.hello_name, name="hello_name"),
-    path('html', views.hello_html, name="hello_html")
+    path('html', views.hello_html, name="hello_html"),
+    path('special', views.special_view, name="special_view"),
+    path('helloquery', views.hello_query, name="hello_query"),
+    path('helloname/<str:name>', views.hello_path, name="hello_name"),
+
 ]
 
 
--- django_project/core/todos/views.py
+++ django_project/core/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,4 +10,13 @@ def hello_name(request):
 
 def hello_html(request):
     return render(request, 'todos/hello.html')
+
+def hello_path(request, name):
+    return HttpResponse(f'Hello, {name}')
+
+def hello_query(request):
+    return HttpResponse(f'your query was {request.GET.get("q")}')
+
+def special_view(request):
+    return redirect('hello_html')
 
